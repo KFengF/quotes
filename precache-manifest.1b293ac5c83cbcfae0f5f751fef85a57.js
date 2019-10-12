6f0ab09ac9e5ce4d8fd9c4d474bbd3d1f0dcb3ea
Kevin Feng <fengfengkevin@gmail.com>
2019-10-12 11:32:01 -0400

    Updates

---
+++ precache-manifest.1b293ac5c83cbcfae0f5f751fef85a57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d519b53f2e54d6b951bc106757081c1",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "cd32122c5216a38af11d",
+    "url": "/quotes/static/css/main.ddb187d3.chunk.css"
+  },
+  {
+    "revision": "db35a502ae15a1d9e63c",
+    "url": "/quotes/static/js/2.e1134b20.chunk.js"
+  },
+  {
+    "revision": "cd32122c5216a38af11d",
+    "url": "/quotes/static/js/main.9550c071.chunk.js"
+  },
+  {
+    "revision": "a8445ba196f4c9bb0d21",
+    "url": "/quotes/static/js/runtime-main.508d9704.js"
+  }
+]);
